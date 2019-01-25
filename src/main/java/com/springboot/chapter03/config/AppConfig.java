3310ba6f6d9833197f99f1f2ddd67e20f0f9d187
rockwang <rockwang@localhost.localdomain>
2019-01-25 16:25:12 +0800

    装配DBCP数据源为DataSource

--- src/main/java/com/springboot/chapter03/config/AppConfig.java
+++ src/main/java/com/springboot/chapter03/config/AppConfig.java
@@ -1,11 +1,17 @@
 package com.springboot.chapter03.config;
 
 import com.springboot.chapter03.pojo.User;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 
 @Configuration  // 1.represents: this is the java config. Spring container use it to set IoC container and set beans
@@ -23,5 +29,21 @@ public class AppConfig {
         user.setUserName("user_name_1");
         return user;
     }
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+        Properties props = new Properties();
+        props.setProperty("driver", "com.mysql.jdbc.Driver");
+        props.setProperty("url", "jdbc:mysql://localhost:3306/chapter03");
+        props.setProperty("username", "root");
+        props.setProperty("password", "123456");
+        DataSource dataSource = null;
+        try {
+            dataSource = BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+	}
 
 }
